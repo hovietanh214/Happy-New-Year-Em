e3ccedfb243ef1b26a1cb00a948bc98aaf27ac9f
hovietanh214 <75459717+hovietanh214@users.noreply.github.com>
2024-02-02 08:12:16 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 const data_name = [
     {
-        ma_ten: "cong",
-        name: "Công",
-        message: "Hello, I'm Công",
+        ma_ten: "anh",
+        name: "Viết Anh",
+        message: "Hello, I'm Vanh",
         img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
     },
     {
-        ma_ten: "thanh",
-        name: "Thanh",
-        message: "Hello, I'm Thanh",
+        ma_ten: "han",
+        name: "Hân",
+        message: "Hello, I'm Han",
         img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
     }
 ]
